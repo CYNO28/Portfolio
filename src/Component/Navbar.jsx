222f55b1b3278fb1dc789cc8f4b412a2c862d65a
Dinesh Singh Bisht <44347226+CYNO28@users.noreply.github.com>
2022-06-28 12:44:50 +0530

    1

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
+import style from './navbar.module.css'
 const Navbar = () => {
   return (
-    <div></div>
+    <>
+      <div className={style.navbar}>
+      <div>
+        <div className={style.navlinks}>
+          <div className={style.navlink}>
+          <p>00.</p>
+          <Link className={style.link}  to={''}>About</Link>
+          </div>
+          <div className={style.navlink}>
+          <p>01.</p>
+          <Link className={style.link} to={''}>Work</Link>
+          </div>
+          <div className={style.navlink}>
+          <p>02.</p>
+          <Link className={style.link} to={''}>Contact</Link>
+          </div>
+        </div>
+      </div>
+      </div>
+    </>
   )
 }
 
