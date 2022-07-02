5f94c8b21210599924f0c061a4e3e3f7d0273596
Dinesh Singh Bisht <44347226+CYNO28@users.noreply.github.com>
2022-07-02 07:31:46 +0530

    1

--- src/Component/Contact/Contactme.jsx
+++ src/Component/Contact/Contactme.jsx
@@ -5,7 +5,7 @@ const Contactme = () => {
     <div style={{color:"white"}} className={style.Contactme}>
         <h4>What's Next ?</h4>
         <h2>Get In Touch</h2>
-    <button>Say Hello</button>
+    <button onClick={()=>{window.open("https://www.linkedin.com/in/dinesh-singh-95a887223/","_blank")}}>Say Hello</button>
     </div>
   )
 }
--- src/Component/Work/Asos.jsx
+++ src/Component/Work/Asos.jsx
@@ -1,37 +1,37 @@
-import React from 'react'
-import { FiGithub } from 'react-icons/fi'
-import { MdOutlineLiveTv } from 'react-icons/md'
+import React from "react";
+import { FiGithub } from "react-icons/fi";
+import { MdOutlineLiveTv } from "react-icons/md";
 import style from "./work.module.css";
 const Asos = () => {
   return (
     <div>
-    <div className={style.workcard}>
-      <img src="/kfc.png" alt="" className={style.img} />
-      <h2 className={style.title}>
-        <a href="https://stalwart-centaur-d742ce.netlify.app/">Asos</a>
-      </h2>
-      <p className={style.description}>
-        A Frontend clone of Asos. using React and Redux.
-      </p>
-      <div className={style.icons}>
-        <a
-          href="https://github.com/CYNO28/ASOS"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FiGithub className={style.icon} />
-        </a>
-        <a
-          href="https://stalwart-centaur-d742ce.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <MdOutlineLiveTv className={style.icon} />
-        </a>
+      <div className={style.workcard}>
+        <img src="/kfc.png" alt="" className={style.img} />
+        <h2 className={style.title}>
+          <a href="https://stalwart-centaur-d742ce.netlify.app/">Asos</a>
+        </h2>
+        <p className={style.description}>
+          A Frontend clone of Asos. using React and Redux.
+        </p>
+        <div className={style.icons}>
+          <a
+            href="https://github.com/CYNO28/ASOS"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FiGithub className={style.icon} />
+          </a>
+          <a
+            href="https://stalwart-centaur-d742ce.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <MdOutlineLiveTv className={style.icon} />
+          </a>
+        </div>
       </div>
-    
-  </div></div>
-  )
-}
+    </div>
+  );
+};
 
-export default Asos
+export default Asos;
