455536ec1d7d28e3d20ac4305ef495a6fd115120
Dinesh Singh Bisht <44347226+CYNO28@users.noreply.github.com>
2022-07-07 13:26:03 +0530

    1

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,41 +3,21 @@ import About from '../Component/About/About'
 import Skills from '../Component/Skills/Skills'
 import Navbar from '../Component/Navbar/Navbar'
 import Sidebar from '../Component/SidebarL/Sidebar'
-import TopModal from '../Component/TopModal'
 import Work from '../Component/Work/Work'
 import Contactme from '../Component/Contact/Contactme'
 import { useRef } from 'react'
 
 const Home = () => {
-  const work_ref = useRef()
-  const about_ref = useRef()
-  const skills_ref = useRef()
-  const contact_ref = useRef()
-function scroller(str){
-  if (str==='work'){
-    work_ref.current.scrollIntoView({behavior: 'smooth'})
-  }
-  if(str==='about'){
-    about_ref.current.scrollIntoView({behavior: 'smooth'})
-  }
-  if(str==='skills'){
-    skills_ref.current.scrollIntoView({behavior: 'smooth'})
-  }
-  if(str==='contact'){
-    contact_ref.current.scrollIntoView({behavior: 'smooth'})
-  }
-  
-}
+
   return (
     <div>
       <Navbar />
-      <About  ref={about_ref} ></About>
-      <Skills ref={skills_ref}></Skills>
-      <Work  ref={work_ref}/> 
+      <About   ></About>
+      <Skills></Skills>
+      <Work /> 
         <Sidebar/>
       
-        <Contactme ref={contact_ref} />
-        {/* <TopModal/> */}
+        <Contactme />
     </div>
   )
 }
