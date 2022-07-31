7459ab2334a696fde5a298b4e81f02655818eef5
Dinesh Singh Bisht <44347226+CYNO28@users.noreply.github.com>
2022-07-31 23:29:27 +0530

    1

--- src/Component/Loading/Load.jsx
+++ src/Component/Loading/Load.jsx
@@ -1,28 +1,31 @@
-import React from 'react'
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import style from './load.module.css'
+import React from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import style from "./load.module.css";
 const Loading = () => {
-  // const navigate=useNavigate()
-  // useEffect(()=>{
-  //   setTimeout(()=>{
-  //     navigate('/home')
-  //   },3000)
-  // })
+  const navigate=useNavigate()
+  useEffect(()=>{
+    setTimeout(()=>{
+      navigate('/home')
+    },6000)
+  })
   return (
-     <div className={style.wrapper}> 
-    <div>
-      <img className={style.icon}  src="/Loading.gif" alt="" />
-    </div>
-    <div className={style.text}>
-    <p>Introverts are like a slow website.<br></br>
-      They might be the coolest site,
-      but usually people don't wait that long for them 
-      to open
-    </p>
+    <div className={style.wrapper}>
+      <div>
+        <img className={style.icon} src="/Loading.gif" alt="" />
+      </div>
+      <div className={style.text}>
+        <p className={style.clrtext}>Introverts are like a slow website.</p>
+        <p className={style.nrtext}>
+          They might be the coolest site,
+          <br></br> but usually people <br></br>don't wait that
+          long for them to open
+        </p>
+        <p className={style.clrtext} style={{marginLeft:"20%",marginTop:"1rem"}}>Extroverts are like Popups</p>
       </div>
     </div>
-  )
-}
+  );
+  
+};
 
-export default Loading
+export default Loading;
