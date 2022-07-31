d24faaa195eaaf9910f5250f532c8d63878e8ad5
Dinesh Singh Bisht <44347226+CYNO28@users.noreply.github.com>
2022-07-31 23:18:26 +0530

    1

--- src/Component/Loading/Load.jsx
+++ src/Component/Loading/Load.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-
+import style from './load.module.css'
 const Loading = () => {
-  const navigate=useNavigate()
-  useEffect(()=>{
-    setTimeout(()=>{
-      navigate('/home')
-    },3000)
-  })
+  // const navigate=useNavigate()
+  // useEffect(()=>{
+  //   setTimeout(()=>{
+  //     navigate('/home')
+  //   },3000)
+  // })
   return (
-    <div style={{width:'100vw',height:'100vh',display:'flex',alignItems:"center",justifyContent:"center"}} >
-      <img src="/Loading.gif" alt="" />
+     <div className={style.wrapper}> 
+    <div>
+      <img className={style.icon}  src="/Loading.gif" alt="" />
+    </div>
+    <div className={style.text}>
+    <p>Introverts are like a slow website.<br></br>
+      They might be the coolest site,
+      but usually people don't wait that long for them 
+      to open
+    </p>
+      </div>
     </div>
   )
 }
