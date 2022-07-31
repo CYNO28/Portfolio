35a899b6954c19a99ea6b3556f6121051bc46dc7
Dinesh Singh Bisht <44347226+CYNO28@users.noreply.github.com>
2022-07-31 21:54:04 +0530

    1

--- src/Component/Projects/Projects.jsx
+++ src/Component/Projects/Projects.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-import Portfolio from './Project/Portfolio'
+import React, { useRef } from 'react'
+import style from './project.module.css'
 
 const Projects = () => {
+  const ref = useRef(null);
+  React.useEffect(() => {
+    window.addEventListener("scroll", () => {
+      
+    });
+  });
   return (
-    <div>
-        <h1></h1>
-        <Portfolio/>
+    <div  className={style.wrapper}>
+      <div className={style.container}>
+        <div className={style.UpperTriangle} ref={ref}></div>
+        <div className={style.BottomTriangle}></div>
+        <div>
+          <h1 style={{color:"white"}}>hello random strnager</h1>
+          </div>
+      </div>
+      <div className={style.container}></div>
+        
+        
     </div>
   )
 }
