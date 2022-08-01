c65e13cf866b74a30d84e29465af023315939bbb
Dinesh Singh Bisht <44347226+CYNO28@users.noreply.github.com>
2022-08-01 20:30:00 +0530

    1

--- src/Component/Projects/Projects.jsx
+++ src/Component/Projects/Projects.jsx
@@ -3,18 +3,23 @@ import React from "react";
 import style from "./project.module.css";
 
 const Projects = () => {
+  const [bol,setbol] = React.useState(false);
   return (
     <div className={style.wrapper}>
       <div className={style.container}>
-        <div className={style.UpperTriangle}></div>
-        <div className={style.BottomTriangle}></div>
-        <div className={style.LeftTriangle}></div>
-        <div className={style.RightTriangle}></div>
+        <div className={style.UpperTriangle} style={bol?{animation: `${style.goup} 2s ease-in-out`}:{}}></div>
+        <div className={style.BottomTriangle} style={bol?{animation: `${style.godown} 2s ease-in-out`}:{}} ></div>
+        <div className={style.LeftTriangle}   style={bol?{animation: `${style.goleft} 2s ease-in-out`}:{}}></div>
+        <div className={style.RightTriangle}  style={bol?{animation: `${style.goright} 2s ease-in-out`}:{}}></div>
         <div>
-          <h1 style={{ color: "white" }}>hello random stranger</h1>
+          <h1 style={{ color: "white" }} onClick={()=>{setbol(!bol)
+         
+          }}> hello random stranger</h1>
         </div>
       </div>
-      <div className={style.container}></div>
+      <div className={style.container}>
+
+      </div>
     </div>
   );
 };
