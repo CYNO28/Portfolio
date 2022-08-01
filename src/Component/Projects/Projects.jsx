b1c4e102645c83600463bbf68c1626d51f37b168
Dinesh Singh Bisht <44347226+CYNO28@users.noreply.github.com>
2022-08-01 19:34:04 +0530

    1

--- src/Component/Projects/Projects.jsx
+++ src/Component/Projects/Projects.jsx
@@ -1,29 +1,22 @@
-import React from 'react'
+import React from "react";
 
-
-import style from './project.module.css'
+import style from "./project.module.css";
 
 const Projects = () => {
-
-
- 
   return (
-    <div  className={style.wrapper}>
-  <div className={style.container}>
-     
+    <div className={style.wrapper}>
+      <div className={style.container}>
         <div className={style.UpperTriangle}></div>
         <div className={style.BottomTriangle}></div>
         <div className={style.LeftTriangle}></div>
         <div className={style.RightTriangle}></div>
         <div>
-          <h1 style={{color:"white"}}>hello random stranger</h1>
-          </div>
+          <h1 style={{ color: "white" }}>hello random stranger</h1>
+        </div>
       </div>
       <div className={style.container}></div>
-        
-        
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
